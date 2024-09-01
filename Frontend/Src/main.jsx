ce2c969adf45de0074370110477953ea08190ee2
Katherine Stehberg <kstehberg@gmail.com>
2024-08-31 20:41:57 -0400

    main.jsx updated

--- Frontend/Src/main.jsx
+++ Frontend/Src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './../Src/pages/'; // Importa el archivo de estilos globales
+import './index.css'; // Corrected the path to import your global styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// Montando la aplicación en el elemento con id 'root' en el index.html
+// Mounting the application in the element with id 'root' in the index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
